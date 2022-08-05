91ce06779a031e4f12ef00738c13c37ed478e802
Emma Batson <emmakbat@gmail.com>
2022-08-05 15:15:24 -0400

    adding scaffolding for SNSPI script

---
+++ spice_elements/snspi/snspi.py
@@ -0,0 +1,23 @@
+from yaml_interface import *
+
+class SNSPI(Element):
+    def generate_asy_content(self, filepath, filename, params=None):
+        pass 
+
+    def lib_generator(self, params):
+        phase_velocity = params['phase_velocity'] # speed of light in material [m/s]
+        length = params['length'] # total length of device [m]
+        num_pixels = params['num_pixels'] # number of discrete pixels in device
+        snpsd_params = params['snspd_params'] # SNSPD parameters (see generate_pixels)
+
+        snspd_statement = self.generate_pixels(snpsd_params)
+
+    def generate_pixels(self, params):
+        Lind = params['Lind'] # kinetic inductance of NW [H]
+        width = params['width'] # width of NW [m]
+        thickness = params['thickness'] # thickness of film [m]
+        sheetRes = params['sheetRes'] # normal sheet resistance [ohms]
+        Tc = params['Tc'] # transition temp [K]
+        Tsub = params['Tsub'] # operating substrate temp [K]
+        Jc = params['Jc'] # critical current density [A]
+        C = params['C'] # constriction factor
---
+++ spice_elements/yaml_interface.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+from sys import platform
+
+import yaml
+import re
+
+import numpy as np
+
+'''
+Functions for interacting with the yaml specification files
+'''
+
+class Element():
+    '''
+    Interface defining some necessary functions each 
+    SPICE element needs to implement differently
+    '''
+    def generate_asy_content(filepath, filename, params):
+        pass 
+
+    def lib_generator(params):
+        pass
+
+def write_yaml(noise_source_dict, dest):
+
+    with open(dest, 'w') as file:
+        yaml.dump(noise_source_dict, file)
+        
+def load_yaml(src):
+    
+    with open(src, 'r') as file:
+        return yaml.load(file, Loader=yaml_loader)
+
+def write_lib(LIB_FILE, content):
+    
+    with open(LIB_FILE, "w") as f:
+        
+        f.write(content)
+        f.close()
+
+def create_asy(filepath, LIB_FILE, name, element):
+    
+    content = element.generate_asy_content(LIB_FILE, name)
+    
+    with open(filepath + name+".asy", "w") as f:
+        
+        f.write(content)
+        f.close()
+
+def file_path(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return os.path.dirname(os.path.abspath(arg)) + "/", \
+               ''.join(os.path.basename(arg).split(".")[:-1]),\
+               os.path.basename(arg).split(".")[-1]
+
+# CUSTOM YAML LOADER
+# 
+# by default, loader doesn't accept 1e5, need 1.e+5...
+# This implies + and .
+
+yaml_loader = yaml.SafeLoader
+yaml_loader.add_implicit_resolver(
+        u'tag:yaml.org,2002:float',
+        re.compile(u'''^(?:
+        [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+        |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+        |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+        |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+        |[-+]?\\.(?:inf|Inf|INF)
+        |\\.(?:nan|NaN|NAN))$''', re.X),
+        list(u'-+0123456789.'))
